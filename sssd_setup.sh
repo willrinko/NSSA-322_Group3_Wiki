1fc4fca27a37b8a801b21aa96468d244c311ec3e
Lily Huntley <89433585+Lily-Huntley@users.noreply.github.com>
2026-09-10 15:55:05 -0400

    Create sssd_setup.sh for SSSD installation and config
    Add SSSD setup script for domain configuration

---
+++ sssd_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+dnf install sssd
+realm join group3l.com
+cat  << EOF
+[domain/group3.com]
+subdomains_homedir = /home/%d/%u
+default_shell = /bin/bash
+fallback_homedir = /home/%u
+
+[domain/group3l.com]
+subdomains_homedir = /home/%d/%u
+default_shell = /bin/bash
+fallback_homedir = /home/%u
+EOF >> /etc/sssd/sssd.conf
+
+authconfig --enablemkhomedir --update
+systemctl enable --now oddjobd.service
